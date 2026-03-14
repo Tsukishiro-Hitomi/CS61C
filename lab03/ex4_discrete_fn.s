d963254d82a4ec0c02559223c0d8d340b627691d
zhaoxinrui <24307140096@fudan.m.edu.cn>
2026-03-14 19:06:50 +0800

    lab03 finished

--- lab03/ex4_discrete_fn.s
+++ lab03/ex4_discrete_fn.s
@@ -6,7 +6,11 @@
 # The return value should be stored in a0
 f:
     # Your code here
-
+    addi t3, a0, 3 # get the correct array index
+    slli t4, t3, 2 # compute the offset, note the size of int is 4
+    add t4, a1, t4 # compute the address, store it in t4
+    lw t5, 0(t4) # get the value, store it in t5
+    mv a0, t5
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra
